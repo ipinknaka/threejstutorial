1e50a1cae2751817746c6c37c48fc6feee13768e
bowkylion10 <bowkylion10@gmail.com>
2023-01-22 13:59:56 +0000

    add menu

--- components/AbsoluteBeginners.tsx
+++ components/AbsoluteBeginners.tsx
@@ -1,4 +1,6 @@
 import { Spinnaker } from '@next/font/google';
+import { useEffect, useReducer, useRef } from 'react';
+import { WebGLRenderer, Scene, PerspectiveCamera, AxesHelper, Raycaster, Vector2 } from 'three';
 
 const spinnaker = Spinnaker({
   weight: '400',
@@ -6,7 +8,39 @@ const spinnaker = Spinnaker({
 })
 
 export default function AbsoluteBeginners() {
-    return (
-        <div id="canvasAbsoluteBeginners" className="w-36 h-36"></div>
-    );
+  const didRunRef = useRef(false);
+  useEffect(() => {
+    if (!didRunRef.current) {
+      didRunRef.current = true;
+      let renderer = new WebGLRenderer({ antialias: true });
+      renderer.setSize(600, 600);
+      const scene = new Scene();
+      const camera = new PerspectiveCamera(75, 1, 0.1, 1000);
+      const axesHelper = new AxesHelper(5);
+      scene.add(axesHelper);
+      camera.position.set(0, 2, 5);
+
+      const animate = () => {
+        // stats.begin();
+        // cube.rotation.x += 0.01;
+        // cube.rotation.y += 0.01;
+        // stats.end();
+        requestAnimationFrame(animate);
+        // cube.rotation.x += 0.01;
+        // cube.rotation.y += 0.01;
+        renderer.render(scene, camera);
+        // statFPS.update();
+        // statMS.update();
+        // statMB.update();
+      }
+      animate();
+
+      const canvas = renderer.domElement as HTMLCanvasElement;
+      document.getElementById("canvasAbsoluteBeginners")?.appendChild(canvas);
+    }
+  }, []);
+
+  return (
+    <div id="canvasAbsoluteBeginners" className="w-[600px] h-[600px] bg-green-300"></div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,42 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
 import { Baloo_2 } from '@next/font/google';
 import { useState } from 'react';
+import AbsoluteBeginners from '@/components/AbsoluteBeginners';
 
 const baloo2 = Baloo_2({
   subsets: ['latin'],
 });
 
 type TopMenuProps = {
+  idx: number;
   title: string;
-  isHighlight: boolean;
+  currentMenuId: number;
 }
-const TopMenu = ({ title, isHighlight }: TopMenuProps) => {
+const TopMenu = ({ idx, title, currentMenuId }: TopMenuProps) => {
   return (
-    <div className={`
-      text-xl
-      ${isHighlight ? 'bg-green-400' : 'bg-slate-300'}
-    `}>
+    <div data-index={idx}
+      className={`cursor-pointer text-xl px-2 rounded-md ${(idx == currentMenuId) ? 'bg-green-400' : 'bg-slate-300'}`}>
       {title}
     </div>
   );
 }
 
 export default function Home() {
-  const [menuId, setMenuId] = useState(1);
+  const [menuId, setMenuId] = useState(0);
+  const topMenuOnClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    //@ts-ignore
+    const ds = e.target.dataset;
+    if ('index' in ds) {
+      setMenuId(ds.index);
+    }
+  }
+  const menu = [
+    'Absolute Beginners',
+    'test2',
+    'test3'
+  ];
+
   return (
     <>
       <Head>
@@ -33,91 +45,21 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={`${baloo2.className} font-light flex flex-row justify-evenly items-center`}>
-        <TopMenu title="Absolute Beginners" isHighlight={false} />
-      </div>
-      <main className={styles.main}>
-
-
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+      <main className="flex flex-col justify-center items-center">
+        <div
+          onClick={topMenuOnClick}
+          className={`${baloo2.className} font-light p-4
+            flex flex-row justify-evenly items-center w-full bg-gray-900`}
+        >
+          {
+            menu.map((m, idx) => <TopMenu key={`topmenu-${idx}`} idx={idx} title={m} currentMenuId={menuId} />)
+          }
         </div>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={baloo2.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={baloo2.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
+          {
+            menuId == 0 && <AbsoluteBeginners />
+          }
 
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={baloo2.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={baloo2.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={baloo2.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={baloo2.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={baloo2.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={baloo2.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
       </main>
     </>
   )
