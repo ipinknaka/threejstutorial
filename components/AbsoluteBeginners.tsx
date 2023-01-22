3872aca5892cf19a3101a868902ee27667597fe5
ipinknaka <ipinknaka@bingzone.net>
2023-01-22 05:57:43 +0000

    test

---
+++ components/AbsoluteBeginners.tsx
@@ -0,0 +1,12 @@
+import { Spinnaker } from '@next/font/google';
+
+const spinnaker = Spinnaker({
+  weight: '400',
+  subsets: ['latin'],
+})
+
+export default function AbsoluteBeginners() {
+    return (
+        <div id="canvasAbsoluteBeginners" className="w-36 h-36"></div>
+    );
+}
